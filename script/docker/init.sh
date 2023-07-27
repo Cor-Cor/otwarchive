85795f6fd33bf5b7c9c7f4187923e7f890cf5dca
Cor-Cor <139630537+Cor-Cor@users.noreply.github.com>
2023-07-27 14:34:48 +0200

    Update init.sh
    edited for new docker compose syntax

--- script/docker/init.sh
+++ script/docker/init.sh
@@ -9,12 +9,12 @@ cp -b config/docker/database.yml config/database.yml
 cp -b config/docker/redis.yml config/redis.yml
 cp -b config/docker/local.yml config/local.yml
 
-docker-compose up -d
+docker compose up -d
 
 sleep 60
 
-docker-compose run --rm web script/reset_database.sh
+docker compose run --rm web script/reset_database.sh
 
 # The development database reset will do everything except run migrations for
 # the test environment:
-docker-compose run --rm test bundle exec rake db:migrate
+docker compose run --rm test bundle exec rake db:migrate
